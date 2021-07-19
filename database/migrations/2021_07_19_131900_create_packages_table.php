c919b5b71ac552aae4b79f917a6b0f4f490b3967
erripollo <pollone.enrico@gmail.com>
2021-07-19 15:53:21 +0200

    create packages migration

--- database/migrations/2021_07_19_131900_create_packages_table.php
+++ database/migrations/2021_07_19_131900_create_packages_table.php
@@ -15,6 +15,14 @@ class CreatePackagesTable extends Migration
     {
         Schema::create('packages', function (Blueprint $table) {
             $table->id();
+            $table->string('cover_image');
+            $table->string('title', 100);
+            $table->text('description');
+            $table->string('destination', 100);
+            $table->mediumInteger('price');
+            $table->string('departure_from', 100);
+            $table->date('departure_date');
+            $table->date('return_date');
             $table->timestamps();
         });
     }
