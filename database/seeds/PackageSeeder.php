ec071725bec464f76445a94b5b6927c5f57cd1a1
erripollo <pollone.enrico@gmail.com>
2021-07-19 16:37:23 +0200

    Create PackageSeeder structure with faker

--- database/seeds/PackageSeeder.php
+++ database/seeds/PackageSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use Faker\Generator as Faker;
+use App\Package;
 use Illuminate\Database\Seeder;
 
 class PackageSeeder extends Seeder
@@ -9,8 +11,20 @@ class PackageSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 10; $i++) { 
+            $package = new Package();
+            
+            $package->cover_image = $faker->imageUrl(640, 480, 'travel', true, null, true);
+            $package->title = $faker->sentence(4);
+            $package->description = $faker->text(300);
+            $package->destination = $faker->city();
+            $package->price = $faker->numberBetween(85, 500);
+            $package->departure_from = $faker->randomElement(['Torino', 'Milano', 'Roma', 'Venezia', 'Napoli', 'Bologna']);
+            $package->departure_date = $faker->date();
+            $package->return_date = $faker->date();
+            $package->save();
+        }
     }
 }
