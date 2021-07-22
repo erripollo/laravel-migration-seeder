dbbd4da0988721daf74c92443a97fb303e0d4888
erripollo <pollone.enrico@gmail.com>
2021-07-22 19:01:00 +0200

    add navbar in layout.app

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -11,7 +11,7 @@
 
         <!-- Fontawesome -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-        
+
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
@@ -19,7 +19,19 @@
     </head>
     <body>
         <header>
-            Site header
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">Travel Agency</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                  <div class="navbar-nav">
+                    <a class="nav-link {{Route::currentRouteName() === 'home' ? 'active' : ''}}" href="{{route('home')}}">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link {{Route::currentRouteName() === 'package.index' ? 'active' : ''}}" href="{{route('package.index')}}">Travel</a>
+                    <a class="nav-link" href="#">Contacts</a>
+                  </div>
+                </div>
+              </nav>
         </header>
         <main>
             @yield('content')
